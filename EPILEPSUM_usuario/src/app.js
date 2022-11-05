f8a5d6d5739aa272e5faedb68707f9ffe7b9723b
ema-munoz <ema.munoz@yavirac.edu.ec>
2022-11-05 18:28:28 -0500

    Estilos 😅

--- EPILEPSUM_usuario/src/app.js
+++ EPILEPSUM_usuario/src/app.js
@@ -61,7 +61,7 @@ app.use((req, res, next) => {
 app.use(express.static(path.join(__dirname, "public")));
 //public
 
-//routas
+//routes
 app.use(require("./routes/index"));
 app.use(require("./routes/registro"));
 app.use(require("./routes/actualizacion"));
